7cae0772ebeda92ff03222bfb1da41d843180c0a
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2022-04-05 21:29:03 +0200

    Changed Auth0 authentication method

--- Server/main.js
+++ Server/main.js
@@ -2,14 +2,27 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
 // Setup Auth0 Middleware Authentication
-const { auth } = require('express-oauth2-jwt-bearer');
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: false,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-q8h6rzir.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://harvest-stalkoverflow.herokuapp.com/',
+  issuer: 'https://dev-q8h6rzir.us.auth0.com/',
+  algorithms: ['RS256']
+});
 
-const checkJwt = auth({
-    audience: 'https://harvest-stalkoverflow.herokuapp.com/',
-    issuerBaseURL: 'https://harvest-stalkoverflow.herokuapp.com/',
-    algorithms: ['RS256']
+app.use(jwtCheck);
+
+// Auth0 authenticated API requests
+app.get('/api/private', function (req, res) {
+    res.send('Authenticated');
 });
 
 // Start listening
@@ -17,33 +30,7 @@ app.listen(port, () => {
     console.log(`Harvest web app listening on port ${port}`)
 })
 
-// Auth0 authenticated API requests
-// public, no login/authentication needed
-app.get('/api/public', function(req, res) {
-    res.json({
-        message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
-    });
-});
-
-// private, requires login/authentication
-app.get('/api/private', checkJwt, function(req, res) {
-    res.json({
-        message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-    });
-});
-
-
-// Regular requests
-app.get('/', (req, res) => {
-    res.send('Time to Harvest!')
-})
-
-app.post('/', (req, res) => {
-    res.send('Got a POST request! ')
-})
-
 if (process.env.CI) {
     console.log("Run in CI, let's stop listening and exit!")
     process.exit()
 }
-//adafsgfhfgdjfg
