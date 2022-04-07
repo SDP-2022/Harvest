dd1a6024a424f29a96988e6b83f4f7b671db16b2
Richard Sims <richievsims@gmail.com>
2022-04-07 21:37:56 +0200

    Update communicator.js

--- Server/communicator.js
+++ Server/communicator.js
@@ -17,7 +17,7 @@ class communicator{
 
 
 //username String, email String, userID String
-addUser(username,email,userID){
+async addUser(username,email,userID){
   await client.connect();
   const text = `INSERT INTO "accounts"("Username","Email","User_ID","Date_Last_Accessed","Date_Joined") values($1,$2,$3,CURRENT_DATE,CURRENT_DATE) RETURNING *`;
   const values = []
@@ -39,7 +39,7 @@ addUser(username,email,userID){
 }
 
 //userID String
-logUser(userID){
+async logUser(userID){
   await client.connect();
   const text = `UPDATE "accounts" SET "Date_Last_Accessed" = CURRENT_DATE WHERE "User_ID"= $1 RETURNING *`;
   const values = []
@@ -59,7 +59,7 @@ logUser(userID){
 }
 
 //userID String, food String
-getWeight(userID,food){
+async getWeight(userID,food){
   await client.connect();
   const text = `SELECT SUM("Weight") FROM "log" WHERE "User_ID" = $1 AND "Food_Name" = $2`;
   const values = []
