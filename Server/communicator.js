25c914fc9d7e55e36e8a4a48828c0f4badc1e0c9
Richard Sims <richievsims@gmail.com>
2022-04-07 21:35:01 +0200

    Update communicator.js

--- Server/communicator.js
+++ Server/communicator.js
@@ -18,14 +18,14 @@ class communicator{
 
 //username String, email String, userID String
 addUser(username,email,userID){
-  client.connect();
+  await client.connect();
   const text = `INSERT INTO "accounts"("Username","Email","User_ID","Date_Last_Accessed","Date_Joined") values($1,$2,$3,CURRENT_DATE,CURRENT_DATE) RETURNING *`;
   const values = []
   values.push(username);
   values.push(email);
   values.push(userID);
   client.query(text, values, (err, res) => {
-    client.end();
+    await client.end();
     if (err) {
       console.log(err.stack);
       return err.stack;
@@ -40,12 +40,12 @@ addUser(username,email,userID){
 
 //userID String
 logUser(userID){
-  client.connect();
+  await client.connect();
   const text = `UPDATE "accounts" SET "Date_Last_Accessed" = CURRENT_DATE WHERE "User_ID"= $1 RETURNING *`;
   const values = []
   values.push(userID);
   client.query(text, values, (err, res) => {
-    client.end();
+    await client.end();
     if (err) {
       console.log(err.stack);
       return err.stack;
@@ -60,13 +60,13 @@ logUser(userID){
 
 //userID String, food String
 getWeight(userID,food){
-  client.connect();
+  await client.connect();
   const text = `SELECT SUM("Weight") FROM "log" WHERE "User_ID" = $1 AND "Food_Name" = $2`;
   const values = []
   values.push(userID);
   values.push(food);
   client.query(text, values, (err, res) => {
-    client.end();
+    await client.end();
     if (err) {
       console.log(err.stack);
       return err.stack
