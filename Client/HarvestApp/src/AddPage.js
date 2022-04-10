82b8b419f264c9019159cf36b26aab2231910dbc
MMMalvin <malvinmaina23@gmail.com>
2022-04-10 22:07:19 +0200

    Added Validation and POST request

--- Client/HarvestApp/src/AddPage.js
+++ Client/HarvestApp/src/AddPage.js
@@ -1,20 +1,64 @@
-import React from 'react'; 
- import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, TextInput } from 'react-native'; 
- 
- const AddPage = () => {
-   return (
-     <SafeAreaView style = {styles.body}>
-       <TextInput style = {styles.input_top} placeholder = 'Search for you item here'/>
-       <TextInput style = {styles.input_bottom} placeholder = 'Weight in grams'/>
-       <View style = {styles.buttonView}> 
-        <TouchableOpacity style = {styles.button}>
-          <Text style = {styles.text}>Add Item</Text>
-        </TouchableOpacity>
-       </View>
-     </SafeAreaView>
-   )
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, { useState } from 'react'; 
+import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, TextInput} from 'react-native'; 
+
+const ACCESS_TOKEN = '@save_token';
+
+ export default function AddPage({navigation, route}){
+  const {userIDToken, userAccessToken, authUsername, userID} = route.params;
+  const [foodType, setFoodType] = useState("");
+  const [weightAmt, setWeightAmt] = useState("");
+
+  
+  const submitData = async()=>{
+    fetch("https://harvest-stalkoverflow.herokuapp.com/api/private/",{
+        method:'POST',
+        headers:{
+          'Content-type': 'application/json',
+          'Authorization' : 'Bearer '  + userAccessToken,
+          'RequestType' : "AddFoodLog"
+        },
+        body:JSON.stringify({
+          UserID : userID,
+	        FoodName : foodType,
+	        Weight : Number(weightAmt)      
+        })
+    })
+    .then(res=>res.text())
+    .then(text=>{
+        console.log(text)
+        console.log("Success")
+    })
+    .catch(error=>{
+      console.log(error)
+      console.log("Failure")
+  });
+}
+
+const checkTextInput = () => {
+  if(!foodType.trim() || !weightAmt.trim()){
+    alert("Please enter all fields")
+    }
+  else{
+    submitData();
+    alert("Produce added to your log.")
+  }
+  };
+
+  return (
+    <SafeAreaView style = {styles.body}>
+      <TextInput style = {styles.input_top} placeholder = 'Search for you item here' onChangeText={(value) => setFoodType(value)}/>
+      <TextInput style = {styles.input_bottom} placeholder = 'Weight in grams' onChangeText={(value) => setWeightAmt(value)}/>
+      <View style = {styles.buttonView}> 
+       <TouchableOpacity style = {styles.button} onPress = {() => checkTextInput()}>
+         <Text style = {styles.text}>Add Item</Text>
+       </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  )
  }
- export default AddPage;
 
  const styles = StyleSheet.create({ 
   body: {
