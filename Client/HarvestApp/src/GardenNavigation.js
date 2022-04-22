4e2d22173595b7333ccf070b1715bb5f69f9f9b5
Shaariq <mshaariq.p@gmail.com>
2022-04-22 13:40:43 +0200

    Completed styling for hamburger menu

--- Client/HarvestApp/src/GardenNavigation.js
+++ Client/HarvestApp/src/GardenNavigation.js
@@ -1,10 +1,10 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {
-    createDrawerNavigator,
-    DrawerContentScrollView,
-    DrawerItemList,
-    DrawerItem,
-  } from "@react-navigation/drawer";
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
@@ -30,35 +30,107 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import GardenPage from "./GardenPage.js";
-import BarGraphPage from "./BarGraphPage.js";
-import PieChartPage from "./PieChartPage.js";
+import GardenPage from './GardenPage.js';
+import BarGraphPage from './BarGraphPage.js';
+import PieChartPage from './PieChartPage.js';
 
 const Drawer = createDrawerNavigator();
 
 export default function GardenNavigation({navigation, route}) {
+  const {userIDToken, userAccessToken, authUsername, userID} = route.params;
 
-    const {userIDToken, userAccessToken, authUsername, userID} = route.params;
-  
+  const CustomDrawer = props => {
     return (
-        <Drawer.Navigator 
-          initialRouteName="GardenPage"
-        >
-          <Drawer.Screen 
-            name="GardenPage" 
-            component={GardenPage} 
-            initialParams={{userIDToken: userIDToken, userAccessToken : userAccessToken, authUsername: authUsername, userID : userID}}
-          />
-          <Drawer.Screen 
-            name="BarGraphPage" 
-            component={BarGraphPage} 
-            initialParams={{userIDToken: userIDToken, userAccessToken : userAccessToken, authUsername: authUsername, userID : userID}}
-          />
-          <Drawer.Screen 
-            name="PieChartPage"
-            component={PieChartPage}
-            initialParams={{userIDToken: userIDToken, userAccessToken : userAccessToken, authUsername: authUsername, userID : userID}}
-          />
-        </Drawer.Navigator>
+      <View style={{flex: 1}}>
+        <DrawerContentScrollView {...props}>
+          <Text style={{color: '#000', fontSize: 20, marginLeft: 20}}>
+            Hey {authUsername}
+          </Text>
+          <View style={{flex: 1, paddingTop: 30}}>
+            <DrawerItemList {...props} />
+          </View>
+        </DrawerContentScrollView>
+      </View>
     );
-  }
+  };
+
+  return (
+    <Drawer.Navigator
+      initialRouteName="GardenPage"
+      screenOptions={{headerShown: false}}
+      drawerContent={props => <CustomDrawer {...props} />}>
+      <Drawer.Screen
+        name="Garden Log"
+        component={GardenPage}
+        initialParams={{
+          userIDToken: userIDToken,
+          userAccessToken: userAccessToken,
+          authUsername: authUsername,
+          userID: userID,
+        }}
+        options={{
+          drawerActiveTintColor: '#000',
+          drawerInactiveTintColor: '#000',
+          drawerActiveBackgroundColor: '#A1E8Af',
+          drawerIcon: () => (
+            <Image
+              style={{
+                height: 20,
+                width: 20,
+              }}
+              source={require('../assets/log-list-icon.png')}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Bar Graphs"
+        component={BarGraphPage}
+        initialParams={{
+          userIDToken: userIDToken,
+          userAccessToken: userAccessToken,
+          authUsername: authUsername,
+          userID: userID,
+        }}
+        options={{
+          drawerActiveTintColor: '#000',
+          drawerInactiveTintColor: '#000',
+          drawerActiveBackgroundColor: '#A1E8Af',
+          drawerIcon: () => (
+            <Image
+              style={{
+                height: 20,
+                width: 20,
+              }}
+              source={require('../assets/bar-graph-icon.png')}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Pie Charts"
+        component={PieChartPage}
+        initialParams={{
+          userIDToken: userIDToken,
+          userAccessToken: userAccessToken,
+          authUsername: authUsername,
+          userID: userID,
+        }}
+        options={{
+          drawerActiveTintColor: '#000',
+          drawerInactiveTintColor: '#000',
+          drawerActiveBackgroundColor: '#A1E8Af',
+          drawerIcon: () => (
+            <Image
+              style={{
+                height: 20,
+                width: 20,
+              }}
+              source={require('../assets/pie-chart-icon.png')}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
--- Client/HarvestApp/src/GardenPage.js
+++ Client/HarvestApp/src/GardenPage.js
@@ -13,8 +13,9 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  Image,
   ActivityIndicator,
-  RefreshControl
+  RefreshControl,
 } from 'react-native';
 
 import {
@@ -34,166 +35,168 @@ export default function GardenPage({navigation, route}) {
   const [FoodLen, setFoodLen] = useState(0);
   const [Loading, setLoading] = useState(true);
   const [Refresh, setRefresh] = useState(false);
-  const [Data, setData] = useState([{key: 1, text: 'text'}])
+  const [Data, setData] = useState([{key: 1, text: 'text'}]);
 
   // This function is used for testing
-  const getVar = async() => {
-    return fetch(
-      'https://harvest-stalkoverflow.herokuapp.com/api/private/',
-      {
-        method: 'GET',
-        headers: {
-          'Authorization' : 'Bearer '  + userAccessToken
-        },
+  const getVar = async () => {
+    return fetch('https://harvest-stalkoverflow.herokuapp.com/api/private/', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + userAccessToken,
       },
-    ).then((response) => response.text())
-    .then((text)=>{
-      console.log(text)
     })
-    .catch((error)=>{
-      console.log(error)
-    })
+      .then(response => response.text())
+      .then(text => {
+        console.log(text);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   // This function gets the log for the current user
-  const getLog = async() => {
-    return fetch(
-      'https://harvest-stalkoverflow.herokuapp.com/api/private/',
-      {
-        method: 'GET', 
-        headers: {
-          'Authorization' : 'Bearer ' + userAccessToken,
-          'RequestType' : 'GetHarvestLogs',
-          'UserID' : userID
-        },
+  const getLog = async () => {
+    return fetch('https://harvest-stalkoverflow.herokuapp.com/api/private/', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + userAccessToken,
+        RequestType: 'GetHarvestLogs',
+        UserID: userID,
       },
-    ).then((response) => response.json())
-    .then((json) => {
-      return json
     })
-    .catch((error) => {
-      console.log(error)
-    })
+      .then(response => response.json())
+      .then(json => {
+        return json;
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   // This function gets the weight of a specific food item
-  const getWeight = async(foodname) => {
-    return fetch(
-      'https://harvest-stalkoverflow.herokuapp.com/api/private/',
-      {
-        method: 'GET',
-        headers: {
-          'Authorization' : 'Bearer ' + userAccessToken,
-          'RequestType' : 'GetFoodTotalWeight',
-          'UserID' : userID,
-          'FoodName' : foodname
-        }
-      }
-    ).then((response) => response.text())
-    .then((text) => {
-      console.log(text)
+  const getWeight = async foodname => {
+    return fetch('https://harvest-stalkoverflow.herokuapp.com/api/private/', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + userAccessToken,
+        RequestType: 'GetFoodTotalWeight',
+        UserID: userID,
+        FoodName: foodname,
+      },
     })
-    .catch((error) => {
-      console.log(error)
-    })
+      .then(response => response.text())
+      .then(text => {
+        console.log(text);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   // This ensures the appropriate functions run when the screen is accessed
   useEffect(() => {
-    getLog()
-    .then((json) => {
-      setLoading(false)
-      console.log(json)
-      setFood(json)
-      setFoodLen(Object.keys(json).length)
-      setRefresh(false)
-    })
+    getLog().then(json => {
+      setLoading(false);
+      console.log(json);
+      setFood(json);
+      setFoodLen(Object.keys(json).length);
+      setRefresh(false);
+    });
   }, [Refresh]);
-  
 
   if (Loading == true) {
-    return(
-      <SafeAreaView style={styles.body}>
-        <ActivityIndicator size="large" color="#A1E8Af"/>
-      </SafeAreaView>
-    )
-  }
-  else if (Loading == false && FoodLen == 0) {
     return (
       <SafeAreaView style={styles.body}>
-  
-        <View style={styles.textView}>
-            <Text style={styles.userText}>Hey {authUsername}</Text>
+        <ActivityIndicator size="large" color="#A1E8Af" />
+      </SafeAreaView>
+    );
+  } else if (Loading == false && FoodLen == 0) {
+    return (
+      <SafeAreaView style={styles.body}>
+        <View style={styles.burgerView}>
+          <TouchableOpacity onPress={navigation.openDrawer}>
+            <Image
+              style={{
+                height: 20,
+                width: 20,
+              }}
+              source={require('../assets/hamburger-icon.png')}
+            />
+          </TouchableOpacity>
         </View>
-  
+
         <FlatList
           refreshControl={
             <RefreshControl
-              refreshing={ Refresh }
+              refreshing={Refresh}
               onRefresh={() => {
-                setRefresh(true)
+                setRefresh(true);
               }}
             />
           }
           showsVerticalScrollIndicator={false}
           style={styles.flatList}
           data={Data}
-          ListHeaderComponent={()=>
+          ListHeaderComponent={() => (
             <View style={styles.header}>
               <Text style={styles.text}>Your Garden is Empty</Text>
               <Text style={styles.text}>¯\_(ツ)_/¯</Text>
             </View>
-          }
+          )}
           // renderItem={({item}) => (
-            // <View style={styles.foodView}>
-            //   <Text style={styles.foodViewText}>{item.key}</Text>
-            //   <Text style={styles.foodViewText}>{item.text}</Text>
-            // </View>
+          // <View style={styles.foodView}>
+          //   <Text style={styles.foodViewText}>{item.key}</Text>
+          //   <Text style={styles.foodViewText}>{item.text}</Text>
+          // </View>
           // )}
         />
-  
+      </SafeAreaView>
+    );
+  } else if (Loading == false && FoodLen > 0) {
+    return (
+      <SafeAreaView style={styles.body}>
+        <View style={styles.burgerView}>
+          <TouchableOpacity onPress={navigation.openDrawer}>
+            <Image
+              style={{
+                height: 20,
+                width: 20,
+              }}
+              source={require('../assets/hamburger-icon.png')}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          refreshControl={
+            <RefreshControl
+              refreshing={Refresh}
+              onRefresh={() => {
+                setRefresh(true);
+              }}
+            />
+          }
+          showsVerticalScrollIndicator={false}
+          style={styles.flatList}
+          data={Food}
+          ListHeaderComponent={() => (
+            <View style={styles.header}>
+              <Text style={styles.text}>Your Garden:</Text>
+            </View>
+          )}
+          renderItem={({item}) => (
+            <View style={styles.foodView}>
+              <Text style={styles.foodViewText}>{item.Food_Name}</Text>
+              <Text style={styles.foodViewText}>Weight: {item.Weight}g</Text>
+              <Text style={styles.foodViewText}>
+                Date: {item.Date_Logged.slice(0, 10)}
+              </Text>
+            </View>
+          )}
+        />
       </SafeAreaView>
     );
   }
-  else if (Loading == false && FoodLen > 0) {
-  return (
-    <SafeAreaView style={styles.body}>
-
-      <View style={styles.textView}>
-          <Text style={styles.userText}>Hey {authUsername}</Text>
-      </View>
-
-      <FlatList
-        refreshControl={
-          <RefreshControl
-            refreshing={ Refresh }
-            onRefresh={() => {
-              setRefresh(true)
-            }}
-          />
-        }
-        showsVerticalScrollIndicator={false}
-        style={styles.flatList}
-        data={Food}
-        ListHeaderComponent={()=>
-          <View style={styles.header}>
-            <Text style={styles.text}>Your Garden:</Text>
-          </View>
-        }
-        renderItem={({item}) => (
-          <View style={styles.foodView}>
-            <Text style={styles.foodViewText}>{item.Food_Name}</Text>
-            <Text style={styles.foodViewText}>
-              Weight: {item.Weight}g
-            </Text>
-            <Text style={styles.foodViewText}>Date: {item.Date_Logged.slice(0, 10)}</Text>
-          </View>
-        )}
-      />
-
-    </SafeAreaView>
-  );
-        }
 }
 
 const styles = StyleSheet.create({
@@ -201,19 +204,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
-  textView: {
+  burgerView: {
     flex: 0.1,
+    paddingTop: 20,
+    marginLeft: 20,
     alignSelf: 'stretch',
   },
   flatList: {
     flex: 2,
-    marginBottom: 50
+    marginBottom: 50,
   },
   text: {
     color: '#000',
-    fontSize: 25
+    fontSize: 25,
   },
   header: {
     alignItems: 'center',
@@ -223,7 +228,7 @@ const styles = StyleSheet.create({
     color: '#000',
     marginLeft: 20,
     fontSize: 20,
-    textAlign: 'left'
+    textAlign: 'left',
   },
   foodView: {
     width: 300,
