7972226ea8df844ea18c7d26cae137c7d6905603
MMMalvin <malvinmaina23@gmail.com>
2022-05-07 18:11:13 +0200

    Added the autocomplete feature
    The autocomplete displays the options but I am still working on the problem I have been having.

--- Client/HarvestApp/src/AddPage.js
+++ Client/HarvestApp/src/AddPage.js
@@ -1,28 +1,33 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, { useState } from 'react'; 
+import React, { useState, useEffect, useCallback } from 'react'; 
 import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, TextInput} from 'react-native'; 
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
 
 const ACCESS_TOKEN = '@save_token';
 
  export default function AddPage({navigation, route}){
   const {userIDToken, userAccessToken, authUsername, userID} = route.params;
-  const [foodType, setFoodType] = useState("");
   const [weightAmt, setWeightAmt] = useState("");
+  const [foodtype, setFoodType] = useState("");
+  const [example, setExample] = useState("");
+  const [remoteDataSet, setRemoteDataSet] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   
-  const submitData = async()=>{
+  const submitData = async()=>{ // this code is used to make a post request to the database and save the data on the database
     fetch("https://harvest-stalkoverflow.herokuapp.com/api/private/",{
         method:'POST',
         headers:{
           'Content-type': 'application/json',
-          'Authorization' : 'Bearer '  + userAccessToken,
-          'RequestType' : "AddFoodLog"
+          'Authorization' : 'Bearer '  + userAccessToken, // the authorization needed by Auth0 for the user 
+          'RequestType' : "AddFoodLog" 
         },
         body:JSON.stringify({
           UserID : userID,
-	        FoodName : foodType,
+	        FoodName : foodtype,
 	        Weight : Number(weightAmt)      
         })
     })
@@ -37,20 +42,85 @@ const ACCESS_TOKEN = '@save_token';
   });
 }
 
-const checkTextInput = () => {
-  if(!foodType.trim() || !weightAmt.trim()){
+const checkTextInput = () => { // this method is used to check if the fields are empty, if they are the process will not go through
+  if(!foodtype.trim() || !weightAmt.trim()){
     alert("Please enter all fields")
     }
   else{
     submitData();
-    alert("Produce added to your log.")
+    alert("Produce added to your log.");
   }
   };
 
+  const handleChange = (value) => {
+    getSuggestions(value);
+    setFoodType(value);
+  }
+
+  const getSuggestions = useCallback(async q => {
+    const filterToken = q.toLowerCase()
+    console.log('getSuggestions', filterToken)
+    if (typeof q !== 'string' || q.length < 3) {
+      setRemoteDataSet(null)
+      return
+    }
+    setLoading(true)
+    const response = await fetch("https://harvest-stalkoverflow.herokuapp.com/api/private/",{
+      method:'GET',
+          headers:{
+            'Content-type': 'application/json',
+            'Authorization' : 'Bearer '  + userAccessToken,
+            'RequestType' : 'GetAllFoodNames'
+          },
+    })
+    .then(
+      data =>
+        new Promise(res => {
+          setTimeout(() => res(data), 2000) // imitate of a long response
+        })
+    )
+    const items = await response.json()
+    //console.log(items[0])
+
+    const suggestions = items
+      .filter(item => item.Food_Name.toLowerCase().includes(filterToken))
+      .map(item => ({
+        id: item.Food_Name,
+        title: item.Food_Name
+      }))
+    setRemoteDataSet(suggestions)
+    setLoading(false)
+  }, [])
+
+
+
   return (
     <SafeAreaView style = {styles.body}>
-      <TextInput style = {styles.input_top} placeholder = 'Search for you item here' onChangeText={(value) => setFoodType(value)}/>
-      <TextInput style = {styles.input_bottom} placeholder = 'Weight in grams' onChangeText={(value) => setWeightAmt(value)}/>
+      <View>
+      <AutocompleteDropdown
+        dataSet={remoteDataSet}
+        closeOnBlur={false}
+        useFilter={false}
+        clearOnFocus={false}
+        inputContainerStyle = {styles.inputContainerStyle}
+        suggestionsListContainerStyle = {styles.suggestionsListContainerStyle}
+        textInputProps={{
+          placeholder: 'Search for your item here',
+          placeholderTextColor: "#808080",
+          style: {
+            color: "black"
+          }
+        }}
+        onSelectItem={setSelectedItem}
+        loading={loading}
+        onChangeText={(value) => handleChange(value)}
+        suggestionsListTextStyle={{
+          color: 'black'
+        }}
+        EmptyResultComponent={<Text style={{ padding: 10, fontSize: 15 }}>Oops ¯\_(ツ)_/¯</Text>}
+      />
+      </View>
+      <TextInput style = {styles.input_bottom} placeholder = 'Weight in grams' placeholderTextColor={"#808080"} onChangeText={(value) => setWeightAmt(value)}/>
       <View style = {styles.buttonView}> 
        <TouchableOpacity style = {styles.button} onPress = {() => checkTextInput()}>
          <Text style = {styles.text}>Add Item</Text>
@@ -84,14 +154,27 @@ const checkTextInput = () => {
     justifyContent: 'center',
     
   },
-  input_top:{
+  suggestionsListContainerStyle:{
+    flex:1,
+    marginTop: 110,
+    borderRadius: 30,
+    padding: 10,
+  },
+  containerStyle:{
+    
+  },
+  inputContainerStyle:{
     borderWidth: 1,
     marginLeft: 45,
     marginRight:45,
-    marginTop: 200,
+    marginTop: 100,
     borderRadius: 30,
     padding: 10,
     borderColor: '#A1E8AF',
+    backgroundColor: "white",
+  },
+  listStyle:{
+    
   },
   input_bottom:{
     borderWidth: 1,
@@ -99,7 +182,7 @@ const checkTextInput = () => {
     marginRight:45,
     marginTop: 50,
     borderRadius: 30,
-    padding: 10,
+    padding: 15,
     borderColor: '#A1E8AF',
   },
  });
