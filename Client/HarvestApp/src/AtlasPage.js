707e3e252199902c1daa09921c20c3dfbf192d78
MMMalvin <malvinmaina23@gmail.com>
2022-05-19 16:18:23 +0200

    Update AtlasPage.js
    Added the autocomplete feature on the AtlasPage

--- Client/HarvestApp/src/AtlasPage.js
+++ Client/HarvestApp/src/AtlasPage.js
@@ -1,8 +1,8 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {Node, useState} from 'react';
-import axios from 'axios';
+import {Node, useState, useCallback } from 'react';
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
 import AtlasBody from '../shared/AtlasBody';
 
 import SearchBox from '../shared/searchbox'
@@ -26,16 +26,89 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const ACCESS_TOKEN = '@save_token';
+
 
 export default function AtlasPage({navigation, route}) {
   const {userIDToken, userAccessToken, authUsername, userID} = route.params;
-  
+  const [remoteDataSet, setRemoteDataSet] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [foodtype, setFoodType] = useState("");
+
+  const getSuggestions = useCallback(async q => {
+    const filterToken = q.toLowerCase()
+    //console.log('getSuggestions', filterToken)
+    if (typeof q !== 'string' || q.length < 3) {
+      setRemoteDataSet(null)
+      return
+    }
+    setLoading(true)
+    const response = await fetch("https://harvest-stalkoverflow.herokuapp.com/api/private/",{
+      method:'GET',
+          headers:{
+            'Content-type': 'application/json',
+            'Authorization' : 'Bearer '  + userAccessToken,
+            'RequestType' : 'GetAllFoodNames'
+          },
+    })
+    .then(
+      data =>
+        new Promise(res => {
+          setTimeout(() => res(data), 2000) // imitate of a long response
+        })
+    )
+    const items = await response.json()
+    //console.log(items[0])
+
+    const suggestions = items
+      .filter(item => item.Food_Name.toLowerCase().includes(filterToken))
+      .map(item => ({
+        id: item.Food_Name,
+        title: item.Food_Name
+      }))
+    setRemoteDataSet(suggestions)
+    console.log(suggestions)
+    setLoading(false)
+  }, [])
+
+  const handleChange = (value) => {
+    getSuggestions(value);
+    setFoodType(value);
+  }
+
+  const handleSelectedChange = (value) =>{
+    if(value){
+      setFoodType(value.title);
+    }
+  }
 
   
   return (
     <SafeAreaView style={styles.body}>
-       
-     
+       <View>
+      <AutocompleteDropdown
+        dataSet={remoteDataSet}
+        closeOnBlur={false}
+        useFilter={false}
+        clearOnFocus={false}
+        inputContainerStyle = {styles.inputContainerStyle}
+        suggestionsListContainerStyle = {styles.suggestionsListContainerStyle}
+        textInputProps={{
+          placeholder: 'Search for your item here',
+          placeholderTextColor: "#808080",
+          style: {
+            color: "black"
+          }
+        }}
+        onSelectItem={handleSelectedChange}
+        loading={loading}
+        onChangeText={(value) => handleChange(value)}
+        suggestionsListTextStyle={{
+          color: 'black'
+        }}
+        EmptyResultComponent={<Text style={{ padding: 10, fontSize: 15 }}>Oops ¯\_(ツ)_/¯</Text>}
+      />
+      </View>
     </SafeAreaView>
   );
 }
@@ -43,8 +116,6 @@ export default function AtlasPage({navigation, route}) {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#fff',
   },
   buttonView: {
@@ -64,5 +135,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 30,
     backgroundColor: '#A1E8AF',
+  },
+  suggestionsListContainerStyle:{
+    flex:1,
+    marginTop: 5,
+    borderRadius: 25,
+    padding: 1,
+  },
+  inputContainerStyle:{
+    borderWidth: 1,
+    marginLeft: 45,
+    marginRight:45,
+    marginTop: 10,
+    borderRadius: 25,
+    right:30,
+    padding: 1,
+    borderColor: '#A1E8AF',
+    backgroundColor: "white",
   },
 });
